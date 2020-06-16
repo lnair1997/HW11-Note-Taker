bae08c39cdf73740c684f8e8f4985e9f73e8fbf3
lathisha <lathisha.n@gmail.com>
2020-06-16 07:16:30 -0700

    req path

--- Develope/server.js
+++ Develope/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //HTML Routes
 app.get("*", function (req, res) {
@@ -12,3 +15,8 @@ app.get("*", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/note.html"));
 });
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
