9f86e370bfd255f344bbe7586c970d18612fdca4
lathisha <lathisha.n@gmail.com>
2020-06-17 10:19:14 -0700

    post method

--- Develope/routes/apiRoutes.js
+++ Develope/routes/apiRoutes.js
@@ -2,9 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const data = require("../db/db.json");
 const router = express.Router();
+let newData = data;
 
 router.get("/notes", function (req, res) {
     res.json(data);
+});
+
+router.post("/notes", function (req, res) {
+    data.push(req.body);
+    res.json(req.body);
+
+    fs.writeFileSync("./db/db.json", newData, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File written successfully\n");
+        };
+    });
+
+
 });
 
 module.exports = router;
