163666e5e8223f0aaee357f3e5af2cfbce422f09
lathisha <lathisha.n@gmail.com>
2020-06-17 09:26:23 -0700

    reference in html

--- Develope/routes/apiRoutes.js
+++ Develope/routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-
 const data = require("../db/db.json");
 const router = express.Router();
+
+router.get("/notes", function (req, res) {
+    res.json(data);
+});
