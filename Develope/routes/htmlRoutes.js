5e85acd5a0d32df5fde1093d864035a7b203997c
lathisha <lathisha.n@gmail.com>
2020-06-17 09:00:28 -0700

    import routes+ express

--- Develope/routes/htmlRoutes.js
+++ Develope/routes/htmlRoutes.js
@@ -1,16 +1,16 @@
-const express = require ("express");
+const express = require("express");
 const path = require("path");
+
 const router = express.Router();
 
-//HTML Routes---------------------------------------------------------------------------
-module.exports = function (app) {
+// HTML Routes---------------------------------------------------------------------------
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/note.html"));
+});
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-    
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/note.html"));
-    });
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-};
+// Exports the router to be used as middleware
+module.exports = router;
--- Develope/server.js
+++ Develope/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require("path");
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const apiRouter = require("./routes/apiRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 
 app.listen(PORT, function () {
