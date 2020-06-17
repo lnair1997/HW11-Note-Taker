d237dc3de2e853381d7aa36fa47ce605f2b98a1c
lathisha <lathisha.n@gmail.com>
2020-06-17 10:27:57 -0700

    data isn't writen in file

--- Develope/routes/apiRoutes.js
+++ Develope/routes/apiRoutes.js
@@ -12,15 +12,13 @@ router.post("/notes", function (req, res) {
     data.push(req.body);
     res.json(req.body);
 
-    fs.writeFileSync("./db/db.json", newData, (err) => {
+    fs.writeFileSync("../db/db.json", JSON.stringify(newData), (err) => {
         if (err) {
             console.log(err);
         } else {
             console.log("File written successfully\n");
         };
     });
-
-
 });
 
 module.exports = router;
