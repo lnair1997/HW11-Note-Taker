cc417457a175413fc3773b592133f544f2382650
lathisha <lathisha.n@gmail.com>
2020-06-18 12:13:02 -0700

    delete and id

--- Develope/routes/apiRoutes.js
+++ Develope/routes/apiRoutes.js
@@ -12,7 +12,7 @@ router.post("/notes", function (req, res) {
     data.push(req.body);
     res.json(req.body);
 
-    fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+    fs.writeFile("db/db.json", JSON.stringify(newData), (err) => {
         if (err) {
             console.log(err);
         } else {
@@ -26,28 +26,13 @@ router.delete("/notes/:id", function (req, res) {
 
     for (let i = 0; i < newData.length; i++) {
         if (id === newData[i].id) {
-            return res.json(true);
+            newData = newData.filter(note => note.id != newData[i].id);
+            return res.send(newData[i]);
         }
     }
-
-    
 });
 
 //1. read db.json
 //2. remove note with ID
 //3. rewrite notes to json file
 module.exports = router;
-
-// app.get("/api/characters/:character", function(req, res) {
-//     const chosen = req.params.character;
-  
-//     console.log(chosen);
-  
-//     for (let i = 0; i < characters.length; i++) {
-//       if (chosen === characters[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-  
-//     return res.json(false);
-//   });
