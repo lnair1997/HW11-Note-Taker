4143312358781063c91491d0997680e85ce43dc6
lathisha <lathisha.n@gmail.com>
2020-06-22 09:52:47 -0700

    added uniqid and it works!

--- Develope/routes/apiRoutes.js
+++ Develope/routes/apiRoutes.js
@@ -1,36 +1,50 @@
 const express = require('express');
 const fs = require('fs');
-const data = require("../db/db.json");
+let data = require("../db/db.json");
 const router = express.Router();
-let newData = data;
+const uniqid = require("uniqid");
 
 router.get("/notes", function (req, res) {
     res.json(data);
 });
 
-router.post("/notes", function (req, res) {
-    data.push(req.body);
-    res.json(req.body);
 
-    fs.writeFile("db/db.json", JSON.stringify(newData), (err) => {
+
+router.post("/notes", function (req, res) {
+
+    const newNote = req.body;
+    newNote.id = uniqid();
+
+    data.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(data), (err) => {
         if (err) {
             console.log(err);
         } else {
             console.log("File written successfully\n");
         };
     });
+
+    res.json(newNote);
 });
+
+
 
 router.delete("/notes/:id", function (req, res) {
-    const id = req.param.id;
+    let notesId = req.params.id;
 
-    for (let i = 0; i < newData.length; i++) {
-        if (id === newData[i].id) {
-            newData = newData.filter(note => note.id != newData[i].id);
-            return res.send(newData[i]);
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == notesId) {
+            data = data.filter(note => note.id != data[i].id);
+            return res.send(data[i]);
         }
     }
+
+    return res.json(false);
 });
+
+
+
 
 //1. read db.json
 //2. remove note with ID
